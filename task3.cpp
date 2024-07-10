a296f758fa79cf394b5054a91cf4ee7db9a65dd6
ShengRen <rensheng0410@outlook.com>
2024-07-10 23:40:59 +0800

    final version

--- task3.cpp
+++ task3.cpp
@@ -26,20 +26,34 @@ typedef struct {
 
 HostList hostList;
 
-void get_local_ip(char *buffer) {
+void get_local_ip_and_subnet_mask(char *ip_buffer, char *mask_buffer) {
   struct ifaddrs *ifap, *ifa;
-  struct sockaddr_in *sa;
+  struct sockaddr_in *sa, *mask;
   getifaddrs(&ifap);
   for (ifa = ifap; ifa; ifa = ifa->ifa_next) {
     if (ifa->ifa_addr->sa_family == AF_INET) {
       sa = (struct sockaddr_in *)ifa->ifa_addr;
+      mask = (struct sockaddr_in *)ifa->ifa_netmask;
       if (strncmp(ifa->ifa_name, "lo", 2)) {
-        strcpy(buffer, inet_ntoa(sa->sin_addr));
+        strcpy(ip_buffer, inet_ntoa(sa->sin_addr));
+        strcpy(mask_buffer, inet_ntoa(mask->sin_addr));
         break;
       }
     }
   }
   freeifaddrs(ifap);
+}
+
+uint32_t ip_to_int(const char *ip) {
+  struct sockaddr_in sa;
+  inet_pton(AF_INET, ip, &(sa.sin_addr));
+  return ntohl(sa.sin_addr.s_addr);
+}
+
+void int_to_ip(uint32_t ip, char *buffer) {
+  struct in_addr in;
+  in.s_addr = htonl(ip);
+  inet_ntop(AF_INET, &in, buffer, INET_ADDRSTRLEN);
 }
 
 void scan_host(char *ip) {
@@ -79,8 +93,8 @@ void scan_host(char *ip) {
 
     FD_ZERO(&fdset);
     FD_SET(sockfd, &fdset);
-    timeout.tv_sec = 1; // 1 second timeout
-    timeout.tv_usec = 0;
+    timeout.tv_sec = 0; // 1 second timeout
+    timeout.tv_usec = 30000;
 
     if (select(sockfd + 1, NULL, &fdset, NULL, &timeout) == 1) {
       int so_error;
@@ -99,18 +113,30 @@ void scan_host(char *ip) {
 }
 
 void scan_network() {
-  char base_ip[INET_ADDRSTRLEN];
-  get_local_ip(base_ip);
-  printf("Local IP: %s\n", base_ip);
+  char local_ip[INET_ADDRSTRLEN];
+  char subnet_mask[INET_ADDRSTRLEN];
+  get_local_ip_and_subnet_mask(local_ip, subnet_mask);
 
-  // Modify base_ip to get the network prefix
-  char *last_dot = strrchr(base_ip, '.');
-  if (last_dot)
-    *last_dot = '\0';
+  printf("Local IP: %s\n", local_ip);
+  printf("Subnet Mask: %s\n", subnet_mask);
 
-  for (int i = 1; i <= 10; i++) { // Reduced number of hosts for testing
+  uint32_t local_ip_int = ip_to_int(local_ip);
+  uint32_t subnet_mask_int = ip_to_int(subnet_mask);
+  uint32_t network_addr_int = local_ip_int & subnet_mask_int;
+  uint32_t broadcast_addr_int = network_addr_int | ~subnet_mask_int;
+
+  char network_addr[INET_ADDRSTRLEN];
+  char broadcast_addr[INET_ADDRSTRLEN];
+  int_to_ip(network_addr_int, network_addr);
+  int_to_ip(broadcast_addr_int, broadcast_addr);
+
+  printf("Network Address: %s\n", network_addr);
+  printf("Broadcast Address: %s\n", broadcast_addr);
+
+  for (uint32_t ip_int = network_addr_int + 1; ip_int < broadcast_addr_int;
+       ip_int++) {
     char target_ip[INET_ADDRSTRLEN];
-    snprintf(target_ip, sizeof(target_ip), "%s.%d", base_ip, i);
+    int_to_ip(ip_int, target_ip);
     printf("Scanning IP: %s\n", target_ip);
     scan_host(target_ip);
   }
